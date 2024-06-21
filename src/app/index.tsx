6e0226a68f3eb2de05f20308ac252548ac65e7b6
Boldi247 <kovacs.boldizsar0321@gmail.com>
2024-06-21 23:08:07 +0200

    shadow added for listitems

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -22,7 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "gainsboro",
+    backgroundColor: "ghostwhite",
     justifyContent: "center",
     padding: 10,
   },
